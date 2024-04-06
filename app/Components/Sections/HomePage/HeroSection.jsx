a97233a45d277f7a12e631bc3bd05733797e7e5c
S. M. Mehedi <104342711+MehediPrime@users.noreply.github.com>
2024-04-06 10:21:59 +0600

    basic structure

--- app/Components/Sections/HomePage/HeroSection.jsx
+++ app/Components/Sections/HomePage/HeroSection.jsx
@@ -1,5 +1,23 @@
 const HeroSection = () => {
-  return <div className="min-h-screen">HeroSection</div>;
+  return (
+    <div className="min-h-screen flex items-center">
+      <div className="max-w-6xl mx-auto lg:grid lg:grid-cols-5">
+        <div className="col-span-3 py-4 lg:py-8 ">
+          <div>Hello!</div>
+          <div>Full Stack Web Developer</div>
+          <div className="flex gap-4">
+            <div className="w-8 mt-3 border-t-2 border-black"></div>
+            <div>
+              Hi, I'm a full stack web developer. I'm proficient in all aspects
+              of web project development, from start to finish.
+            </div>
+          </div>
+          <div>Button Section</div>
+        </div>
+        <div className="col-span-2 py-4 lg:py-8 ">2 HeroSection</div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
---
+++ app/Components/test/DesignStructure.jsx
@@ -0,0 +1,7 @@
+// For section ---->
+<div className="min-h-screen">
+  <div className="max-w-6xl mx-auto md:grid md:grid-cols-5">
+    <div className="col-span-3">Section part 1</div>
+    <div className="col-span-2">Section part 2</div>
+  </div>
+</div>;
